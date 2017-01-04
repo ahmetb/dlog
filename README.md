d5f972dc0639d35baf9d72c9e0b5050e1a27e19e
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2017-01-04 11:56:29 -0800

    Add example_test.go and README
    Signed-off-by: Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # dlog
-Go library to read Docker Logs stream
+
+Go library to parse the binary Docker Logs stream into plain text.
+
+`dlog` offers a single method: `NewReader(r io.Reader) io.Reader`. You are
+supposed to give the response body of the `/containers/<id>/logs`. The returned
+reader strips off the log headers and just gives the plain text to be used.
+
+You can get the logs stream from [go-dockerclient][gocl]'s `Logs()`[gocl-logs] method,
+or by calling the endpoint via the UNIX socket directly.
+
+See [`example_test.go`][./example_test.go] for an example usage.
+
+[gocl]: https://github.com/fsouza/go-dockerclient
+[gocl-logs]: https://godoc.org/github.com/fsouza/go-dockerclient#Client.Logs
+
+-----
+
+Licensed under Apache 2.0. Copyright 2016 [Ahmet Alp Balkan][ab].
+
+[ab]: https://ahmetalpbalkan.com/
---
+++ example_test.go
@@ -0,0 +1,47 @@
+package dlog
+
+import (
+	"bufio"
+	"context"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/ahmetalpbalkan/dlog"
+)
+
+func ExampleNewReader() {
+	conn, err := net.DialUnix("unix", nil, &net.UnixAddr{
+		Name: "/var/run/docker.sock",
+		Net:  "unix"})
+	if err != nil {
+		log.Fatalf("cannot connect docker socket: %v", err)
+	}
+
+	client := &http.Client{Transport: &http.Transport{
+		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			return conn, nil
+		}}}
+
+	url := "http://-/containers/CONTAINER_NAME/logs?stdout=1&stderr=1&follow=1"
+	resp, err := client.Get(url)
+	if err != nil {
+		log.Fatalf("request failed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("unexpected status code: %s", resp.Status)
+	}
+
+	// At this point we have a logs stream, here is how to read each log line
+	// from container:
+	r := dlog.NewReader(resp.Body)
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		log.Println(s)
+	}
+	if err := s.Err(); err != nil {
+		log.Fatalf("read error: %v", err)
+	}
+}
